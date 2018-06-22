36b647669d996dac1a70291175f4106de8eb962b
chenzhipeng <chenzhipeng@huajiao.tv>
2018-06-22 10:50:59 +0800

    first

--- .build/x86_64-apple-macosx10.10/debug/CNIOLinux.build/shim.c.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/chenzhipeng/Desktop/server/project1/.build/checkouts/swift-nio.git--8234561371585672779/Sources/CNIOLinux/shim.c
--- .build/x86_64-apple-macosx10.10/debug/CNIOOpenSSL.build/empty.c.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/chenzhipeng/Desktop/server/project1/.build/checkouts/swift-nio-ssl.git--6013054502337919299/Sources/CNIOOpenSSL/empty.c
