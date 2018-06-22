47b06c347a578a20c2eb5ff87bcfa47919baa8d5
chenzhipeng <chenzhipeng@huajiao.tv>
2018-06-22 14:12:12 +0800

    test

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -48,4 +48,28 @@ public func routes(_ router: Router) throws {
         // render the template with whatever we have
         return try req.view().render("staff", context)
     }
+
+    router.get("staff") { req -> Future<View> in
+        // create some dummy data to work with
+        let bios = [
+            "kirk": "My name is James Kirk and I love snakes.",
+            "picard": "My name is Jean-Luc Picard and I'm mad for fish.",
+            "sisko": "My name is Benjamin Sisko and I'm all about the budgies.",
+            "janeway": "My name is Kathryn Janeway and I want to hug every hamster.",
+            "archer": "My name is Jonathan Archer and beagles are my thing."
+        ]
+        
+        // define the struct we'll pass to the template
+        struct StaffView: Codable {
+            var name: String?
+            var bio: String?
+            var allNames: [String]
+        }
+        
+        let context = StaffView(name: nil, bio: nil, allNames: bios.keys.sorted())
+
+        // render the template with whatever we have
+        return try req.view().render("staff", context)
+    }
+
 }
